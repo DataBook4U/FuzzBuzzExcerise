7cc99aaec9bae4dc6390af3aa6eb78f1fed04956
Sebastian Opocensky <opocensky.sebastian@outlook.de>
2025-05-13 16:23:33 +0200

    Changed previous solution into a function

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 #FuzzBuzz Exercise:
 
-for i in range(0, 100):
-    if i % 3 == 0 and  i % 5 == 0:
-        print("FuzzBuzz")
-    elif i % 5 == 0:
-        print("Buzz")
-    elif i % 3 == 0:
-        print("Fuzz")
-    else:
-        print(i)
+def FuzzBuzz(Start, Ende):
+    for i in range(Start, Ende):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FuzzBuzz")
+        elif i % 3 == 0:
+            print("Fuzz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+
+FuzzBuzz(1, 1000)
