a96f5b7ce5edfbe959d6efa9717ab78f24bdd446
Sebastian Opocensky <opocensky.sebastian@outlook.de>
2025-05-13 16:39:22 +0200

    Implemented the previous function into a class

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 #FuzzBuzz Exercise:
 
-def FuzzBuzz(Start, Ende):
-    for i in range(Start, Ende):
-        if i % 3 == 0 and i % 5 == 0:
-            print("FuzzBuzz")
-        elif i % 3 == 0:
-            print("Fuzz")
-        elif i % 5 == 0:
-            print("Buzz")
-        else:
-            print(i)
+class Zahlenspiel:
+    def __init__(self, Startbereich, Endbereich):
+        self.Startbereich = Startbereich
+        self.Endbereich = Endbereich
 
-FuzzBuzz(1, 1000)
+    def FuzzBuzzRätsel(self):
+        for i in range(self.Startbereich, self.Endbereich):
+            if i % 3 == 0 and i % 5 == 0:
+                print("FuzzBuzz")
+            elif i % 3 == 0:
+                print("Fuzz")
+            elif i % 5 == 0:
+                print("Buzz")
+            else:
+                print(i)
+
+aufgabe = Zahlenspiel(1, 20)
+aufgabe.FuzzBuzzRätsel()
